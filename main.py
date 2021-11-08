31bf28c2689c1a3b44b29af50b1b6920a03b4b6b
Oleg Rudakov <nefoser@gmail.com>
2021-11-08 23:44:25 +0300

    Add function get_pictures_url

--- main.py
+++ main.py
@@ -12,9 +12,19 @@ def uploade_pictures(url, filename):
 
     return file
 
-if __name__ == "__main__":
-    Path("files").mkdir(parents=True, exist_ok=True)
-    filename = "files/hubble.jpeg"
-    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
-    uploade_pictures(url, filename)
+def get_pictures_url(request_url):
+    response = requests.get(request_url)
+    response.raise_for_status()
+
+    return response.json()["links"]["flickr_images"]
+
+
+if __name__ == "__main__":
+    Path("images").mkdir(parents=True, exist_ok=True)
+    filename = "images/hubble.jpeg"
+    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    request_url = "https://api.spacexdata.com/v3/launches/67"
+
+    # uploade_pictures(url, filename)
+    print(get_pictures_url(request_url))
