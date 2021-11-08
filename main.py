5b83536e877726166954054622dccc4c9c5cd602
Oleg Rudakov <nefoser@gmail.com>
2021-11-08 17:11:03 +0300

    Add function uploade_pictures

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import requests
+from pathlib import Path
 
 
-url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
-filename = 'hubble.jpeg'
-response = requests.get(url)
-response.raise_for_status()
+def uploade_pictures(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
 
-with open(filename, 'wb') as file:
-    file.write(response.content)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+    return file
+
+if __name__ == "__main__":
+    Path("files").mkdir(parents=True, exist_ok=True)
+    filename = "files/hubble.jpeg"
+    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+
+    uploade_pictures(url, filename)
